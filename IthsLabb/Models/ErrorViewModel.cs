71889ebac62a771fcaf40cc24d19e9014261e0d2
Marcus S. Olkerud <marcus.olkerud@consid.se>
2020-04-20 10:18:27 +0200

    Add project files.

---
+++ IthsLabb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IthsLabb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
